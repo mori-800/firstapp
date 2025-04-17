23fcaae9ff680b6186f7b640b14a1e214e3b6e92
AzureAD\moritaizo <mori.taizo@blueship.co.jp>
2025-04-17 17:24:46 +0900

    不明

--- controllers/posts/allPosts.js
+++ controllers/posts/allPosts.js
@@ -1,7 +1,11 @@
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+
 const prisma = new PrismaClient();
 
-module.exports = async (req, res) => {
+export default async (req: Request, res: Response): Promise<void> => {
   const posts = await prisma.post.findMany();
-  res.render("posts/allPosts", {posts});
-};
+  const test = "テスト文字列";
+
+  res.render("posts/allPosts", { posts, test });
+}
